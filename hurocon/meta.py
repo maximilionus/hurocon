339f58ad1970723b66f7df941717e340c39d2f34
maximilionus <maximilionuss@gmail.com>
2022-04-24 21:23:47 +0300

    Switched to `poetry` base

--- hurocon/meta.py
+++ hurocon/meta.py
@@ -1,2 +1,11 @@
-version = '0.0.1'
+import sys
+
+
+if sys.version_info[:2] >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
+
+version = metadata.version(__package__)
 author = 'maximilionus'
