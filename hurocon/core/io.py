7e47857ad395af1167bff683ff89f0ea1dbcd919
maximilionus <maximilionuss@gmail.com>
2022-07-06 02:19:34 +0300

    Docs for `.core.io` functions

--- hurocon/core/io.py
+++ hurocon/core/io.py
@@ -3,7 +3,11 @@ import textwrap
 
 
 def printx(*text, limit_line_length: bool = False):
-    """ Enhanced long-text printing function """
+    """
+    Enhanced long-text printing function
+
+    :param limit_line_length: Split line by length, defaults to False
+    """
     final_text = ''
     for line in text:
         line_processed = str(line)
@@ -13,6 +17,15 @@ def printx(*text, limit_line_length: bool = False):
 
 
 def prettify(text) -> str:
+    """
+    Prettify the text
+
+    Features:
+    - Split text by length
+
+    :param text: Text to process
+    :return: Formatted string, ready for printing
+    """
     return textwrap.fill(
         text,
         width=80,
